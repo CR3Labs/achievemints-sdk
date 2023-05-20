d2ca5f7f2ea0514f1ecc98c3a2ff6fa0a9b3d1df
mikehroth <60800753+mikehroth@users.noreply.github.com>
2023-05-20 01:03:27 -0600

    chore: update to readme

--- python/example.py
+++ python/example.py
@@ -9,13 +9,13 @@ sdk = AchievemintsSDK(api_key=key)
 resources = sdk.make_request('/game') # will send a GET request to the resources endpoint
 logger.info("Get resources:\n{}".format(resources))
 
-# Add a player to a game
-#  This ensures on-chain events are watched for this player/game combination
-game_id = resources[0]['id']
-player = sdk.make_request('/game/{}/player'.format(game_id), method="POST", body={"wallet": "0x0"})
+# Enable an integration
+#  This ensures on-chain events are watched for a specific type of integration
+# TBD: needs to be done in the UI
 
 # create a subscription
 # to listen to your game events
-subscription = sdk.subscribe("fdd74c25-e7bf-42da-b6e7-d4b5f9ec95d1")
+game_id = resources[0]['id']
+subscription = sdk.subscribe(game_id)
 for event in subscription.events():
         pprint.pprint(json.loads(event.data))
